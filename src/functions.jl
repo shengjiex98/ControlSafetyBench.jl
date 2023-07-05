7f16ab67f10698a1a47bcc7a6ed34ef37f8ed34a
shengjiex98 <shengjiex98@gmail.com>
2023-07-05 14:29:37 -0400

    Specifying state-space system types

--- src/functions.jl
+++ src/functions.jl
@@ -1,23 +1,23 @@
 using ControlSystemsBase
 using LinearAlgebra
 
-function delay_lqr(sys::StateSpace, h::Float64; Q=I, R=I)
-    sysd_delay = c2da(sys, h)
+function delay_lqr(sysc::AbstractStateSpace{<:Contiuous}, h::Float64; Q=I, R=I)
+    sysd_delay = c2da(sysc, h)
     lqr(sysd_delay, Q, R)
 end
 
 # Pole placement only works with single input single output (SISO) systems. i.e., 
 # sys.B & sys.D can only have 1 column and sys.C & sys.D can only have 1 row
-function pole_place(sys::StateSpace, h::Float64; p=0.9)
-    sysd_delay = c2da(sys, h)
+function pole_place(sysc::AbstractStateSpace{<:Contiuous}, h::Float64; p=0.9)
+    sysd_delay = c2da(sysc, h)
     place(sysd_delay, vcat([0], fill(p, size(sys.A)[1])))
 end
 
-function c2da(sys::StateSpace, h::Float64)
-    augment(c2d(sys, h))
+function c2da(sysc::AbstractStateSpace{<:Continuous}, h::Float64)
+    augment(c2d(sysc, h))
 end
 
-function augment(sysd)
+function augment(sysd::AbstractStateSpace{<:Discrete})
     p = size(sysd.A, 1)
     q = size(sysd.B, 2)
     r = size(sysd.C, 1)
