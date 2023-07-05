e3d230ca20c3b032f7fa716d64a0cdc5d25dd974
shengjiex98 <shengjiex98@gmail.com>
2023-07-05 18:40:29 -0400

    Fix typo in functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -1,14 +1,14 @@
 using ControlSystemsBase
 using LinearAlgebra
 
-function delay_lqr(sysc::AbstractStateSpace{<:Contiuous}, h::Float64; Q=I, R=I)
+function delay_lqr(sysc::AbstractStateSpace{<:Continuous}, h::Float64; Q=I, R=I)
     sysd_delay = c2da(sysc, h)
     lqr(sysd_delay, Q, R)
 end
 
 # Pole placement only works with single input single output (SISO) systems. i.e., 
 # sys.B & sys.D can only have 1 column and sys.C & sys.D can only have 1 row
-function pole_place(sysc::AbstractStateSpace{<:Contiuous}, h::Float64; p=0.9)
+function pole_place(sysc::AbstractStateSpace{<:Continuous}, h::Float64; p=0.9)
     sysd_delay = c2da(sysc, h)
     place(sysd_delay, vcat([0], fill(p, size(sys.A)[1])))
 end
